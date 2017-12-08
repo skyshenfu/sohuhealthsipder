f13d28c6a021a8cc2fb3f9c8062ce28404684401
pain <965819494@qq.com>
2017-12-09 01:25:31 +0800

    yyb爬虫

--- souhuselenium.py
+++ souhuselenium.py
@@ -7,6 +7,8 @@ import codecs
 
 import csv
 
+from selenium.common.exceptions import NoSuchElementException
+
 
 class selTest(unittest.TestCase):
     def setUp(self):
@@ -14,23 +16,32 @@ class selTest(unittest.TestCase):
 
     def testEle(self):
         driver = self.driver
-        driver.get("http://health.sohu.com/")
-        for i in range(100):
+        driver.get("http://sj.qq.com/myapp/search.htm?kw=%E8%B5%84%E6%9C%AC")
+        for i in range(50):
             driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            time.sleep(1)
+           # if driver.find_element_by_link_text(u'没有更多了').
+            if (len(driver.find_elements_by_link_text(u'加载更多'))!=0):
+                driver.find_element_by_link_text(u'加载更多').click()
+                print i
+                continue
+            time.sleep(2)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        divs = soup.find_all("div", {"data-role": "news-item"})
+        alist = soup.find_all("a", {"class": "installBtn"})
         # print "http:"+div.a['href']
         # print div.a.string
-        with open('./data/result.csv', 'w') as csvfile:
+        with open('./data/关键字资本.csv', 'w') as csvfile:
             csvfile.write(codecs.BOM_UTF8)
-            fieldnames = ['title', 'srcurl']
+            fieldnames = ['name', 'icon','url','redirecturl','package']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            for div in divs:
-                strtitle = div.h4.a.string.strip().encode("utf-8")
-                strurl = "http:" + div.h4.a['href']
-                writer.writerow({'title': strtitle, 'srcurl': strurl})
+            for a in alist:
+                print a
+                name = a['appname'].encode("utf-8")
+                icon=a['appicon']
+                package=a['apk']
+                url="http://sj.qq.com/myapp/detail.htm?apkName="+package
+                redirecturl=a['ex_url'].encode("utf-8")
+                writer.writerow({'name': name,'icon':icon,'package':package,'url':url,'redirecturl':redirecturl})
 
     def tearDown(self):
         print ''
